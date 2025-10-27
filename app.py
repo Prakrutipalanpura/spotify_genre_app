76698816105211c6149f658be0846e234fdd0736
Prakrutipalanpura <155126242+Prakrutipalanpura@users.noreply.github.com>
2025-10-27 16:59:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
 >>> import streamlit as st
 ... import pandas as pd
 ... import matplotlib.pyplot as plt
@@ -62,5 +60,6 @@ if song_name:
         st.error(f"❌ {str(e)}")
 
 st.markdown("---")
+
 
 
